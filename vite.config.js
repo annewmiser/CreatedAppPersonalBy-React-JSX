dacb2eb65735e5f976cc4221329e9471ebdb5403
annewmiser <rungsan4199@gmail.com>
2025-09-10 04:03:25 +0700

    fix vites

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/CreatedAppBy-React-JSX/',
+  base: '/CreatedAppPersonalBy-React-JSX/',
   plugins: [react(),tailwindcss()],
 })
